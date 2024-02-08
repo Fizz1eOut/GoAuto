7afeb6d574c9bf3f281131b260a76023cf5d0b92
FizzleOut <fizzleout1337@gmail.com>
2024-02-08 19:57:05 +0100

    ThePartners

---
+++ src/components/Content/ThePartners.vue
@@ -0,0 +1,76 @@
+<script>
+import { defineComponent } from 'vue';
+import IconBridgestone from '@/components/icons/IconBridgestone.vue';
+import IconGoodyear from '@/components/icons/IconGoodyear.vue';
+import IconMichelin from '@/components/icons/IconMichelin.vue';
+import AppTitle from '@/components/Base/AppTitle.vue'
+
+export default defineComponent({
+  name: 'ThePartners',
+  
+  components: {
+    IconMichelin,
+    IconBridgestone,
+    IconGoodyear,
+    AppTitle
+},
+});
+</script>
+
+<template>
+  <div class="partners">
+    <app-title>Партнери</app-title>
+    <div class="partners__items">
+      <div class="partners__item">
+        <icon-michelin />
+      </div>
+
+      <div class="partners__item">
+        <icon-bridgestone />
+      </div>
+
+      <div class="partners__item">
+        <icon-goodyear />
+      </div>
+
+      <div class="partners__item">
+        <icon-michelin />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.partners {
+  margin-top: 120px;
+}
+.partners__items {
+  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 70px;
+  overflow-x: auto;
+  padding-bottom: 10px;
+}
+.partners__item {
+  min-width: 250px;
+  width: 100%;
+}
+@media (max-width: 991px) {
+  .partners {
+    margin-top: 100px;
+  }
+}
+
+@media (max-width: 768px) {
+  .partners {
+    margin-top: 70px;
+  }
+}
+@media (max-width: 499px) {
+  .partners__items {
+    margin-top: 40px;
+  }
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,6 +2,7 @@
 import { defineComponent } from 'vue';
 import TheOurAdvantages from '@/components/Content/TheOurAdvantages.vue';
 import TheConsultation from '@/components/Content/TheConsultation.vue';
+import ThePartners from '@/components/Content/ThePartners.vue';
 
 export default defineComponent({
   name: 'HomeView',
@@ -9,14 +10,16 @@ export default defineComponent({
   components: {
     TheOurAdvantages,
     TheConsultation,
-  },
+    ThePartners
+},
 
 });
 </script>
 
 <template>
-  <the-our-advantages></the-our-advantages>
-  <the-consultation></the-consultation>
+  <the-our-advantages />
+  <the-consultation />
+  <the-partners />
 </template>
 
 <style scoped>
