a6eabdae0a62e5290a270dc209225d3188375323
FizzleOut <fizzleout1337@gmail.com>
2024-03-03 19:33:43 +0100

    created AppSlider and AppProduct components

---
+++ src/components/Base/AppProduct.vue
@@ -0,0 +1,129 @@
+<script>
+import AppContainer from '@/components/Base/AppContainer.vue';
+import AppUnderlay from '@/components/Base/AppUnderlay.vue';
+import AppButton from '@/components/Base/AppButton.vue';
+import { defineComponent } from 'vue';
+import IconCart from '@/components/icons/IconCart.vue';
+
+export default defineComponent({
+  name: 'AppProduct',
+
+  components: {
+    AppContainer,
+    AppUnderlay,
+    AppButton,
+    IconCart,
+  },
+  
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    article: {
+      type: String,
+      required: true
+    },
+    stock: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      added: false,
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.added = !this.added;
+    }
+  }
+});
+</script>
+
+<template>
+  <app-underlay>
+    <app-container size="sm">
+      <div class="product">
+        <div class="product__img">
+          <img :src="imageUrl" alt="image">
+        </div>
+
+        <div class="product__content">
+          <div class="product__title">{{ title }}</div>
+          <div class="product__article"> Артикул: {{ article }} </div>
+          <div class="product__stock">{{ stock }}</div>
+        </div>
+        <div class="product__row">
+          <div class="product__price">{{ price }}₴</div>
+          <app-button :disabled="added" @click="buttonClick">
+            Купити
+            <IconCart class="icon" />
+          </app-button>
+        </div>
+      </div>
+    </app-container>
+  </app-underlay>
+</template>
+
+<style>
+  .product {
+  }
+  .product__img img {
+    max-width: 266px;
+    max-height: 266px;
+  }
+  .product__content {
+    margin-top: 20px;
+  }
+  .product__content> *:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  .product__title {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 160%;
+    color: var(--color-black);
+  }
+  .product__article {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 160%;
+    color: var(--color-black);
+  }
+  .product__stock {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 160%;
+    color: var(--color-green);
+  }
+  .product__row {
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+  }
+  .product__price {
+    width: 50%;
+    font-weight: 700;
+    font-size: 24px;
+    color: var(--color-black);
+  }
+  .icon {
+    margin-left: 8px;
+    width: 12px;
+    height: 12px;
+    fill: var(--color-white);
+  }
+</style>
---
+++ src/components/Base/AppSlider.vue
@@ -0,0 +1,164 @@
+<script>
+import AppProduct from '@/components/Base/AppProduct.vue'
+import AppTitle from '@/components/Base/AppTitle.vue'
+import { Splide, SplideSlide, SplideTrack} from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppSlider',
+
+  components: {
+    Splide,
+    SplideSlide,
+    SplideTrack,
+    AppTitle,
+    AppProduct,
+  },
+  
+  data() {
+    return {
+      slideOptions: {
+        type: 'slide',
+        pagination: true,
+        arrows: false,
+        paginationKeyboard: true,
+        perPage: 4,
+        breakpoints: {
+          640: {
+            perPage: 2,
+          },
+        }
+      },
+    }
+  },
+});
+</script>
+
+<template>
+  <div class="slider">
+    <app-title>
+      Рекомендовані товари
+    </app-title>
+
+    <div class="slider__body">
+      <Splide 
+        :options="slideOptions" 
+        :has-track="false" 
+      >
+        <SplideTrack>
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+
+          <SplideSlide>
+            <app-product 
+              image-url="https://www.oponeo.pl/Temp/michelin-pilot-alpin-5-suv-18806-234724-f-f-l600-sk3.webp"
+              title="Michelin X-Ice Snow"
+              article="MXIS001"
+              stock="В наявності"
+              price="4 900"
+            />
+          </SplideSlide>
+        </SplideTrack>
+      </Splide>
+    </div>
+  </div>
+</template>
+
+<style>
+  .slider {
+    margin-top: 120px;
+  }
+  .slider__body {
+    margin-top: 60px;
+  }
+  .slider__body .splide__pagination {
+    bottom: -50px;
+  }
+  .splide__pagination {
+    gap: 10px;
+  }
+  .slider__body .splide__pagination .splide__pagination__page {
+    background-color: var(--color-grey);
+  }
+
+  .slider__body .splide__pagination .splide__pagination__page.is-active {
+    background-color: var(--color-blue);
+  }
+</style>
