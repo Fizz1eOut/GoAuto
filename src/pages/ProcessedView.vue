45ac3bad85e39e37ddfd0d3e07a8f9df3d4335fb
FizzleOut <fizzleout1337@gmail.com>
2024-03-26 19:10:02 +0100

    created a product registration page

--- src/pages/ProcessedView.vue
+++ src/pages/ProcessedView.vue
@@ -1,21 +1,53 @@
 <script>
 import { defineComponent } from 'vue';
-
+import IconCar from '@/components/icons/IconCar.vue';
+import AppTitle from '@/components/Base/AppTitle.vue';
 
 export default defineComponent({
   name: 'ProcessedView',
 
   components: {
-
+    IconCar,
+    AppTitle
 },
 
 });
 </script>
 
 <template>
-  adsasdasssssssssssssssssssssssssssssssssssssssss
+  <div class="order-processed">
+    <div class="order-precessed__body">
+      <icon-car class="icon" />
+      <app-title>
+        Дякуємо за замовлення!
+      </app-title>
+      <p class="order-processed__text">Ваше замовлення 5538 оформлено.<br>Менеджер незабаром зв'яжеться з вами.</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+  .icon {
+    width: 264px;
+    height: 103px;
+  }
+  .order-processed {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .order-precessed__body> *:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  .order-processed__text {
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 150%;
+    text-align: center;
+    color: var(--color-black);
+  }
+  @media (max-width: 499px) {
 
+  }
 </style>
