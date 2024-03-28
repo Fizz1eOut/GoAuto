1f8efc602b88473bd08e74d39e64e5bd715ac037
FizzleOut <fizzleout1337@gmail.com>
2024-03-28 18:41:47 +0100

    changed AppPages component

---
+++ src/components/Base/AppBreadcrumbs.vue
@@ -0,0 +1,39 @@
+<script>
+// import PagesItem from '@/components/Content/PagesItem.vue'
+// import AppLink from '@/components/Base/AppLink.vue';
+// import IconArrowRight from '@/components/icons/IconArrowRight.vue'
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppBreadcrumbs',
+
+  components: {
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  }
+});
+</script>
+
+<template>
+  <div class="pages">
+    <div class="pages__items">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .pages__items {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+</style>
--- src/pages/CartView.vue
+++ src/pages/CartView.vue
@@ -1,14 +1,20 @@
 <script>
-import TheCheckout from '@/components/Content/TheCheckout.vue'
-import AppPages from '@/components/Base/AppPages.vue';
+import PagesItem from '@/components/Content/PagesItem.vue';
+import AppLink from '@/components/Base/AppLink.vue';
+import IconArrowRight from '@/components/icons/IconArrowRight.vue';
+import TheCheckout from '@/components/Content/TheCheckout.vue';
+import AppBreadcrumbs from '@/components/Base/AppBreadcrumbs.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'CartView',
 
   components: {
-    AppPages,
+    AppBreadcrumbs,
     TheCheckout,
+    PagesItem,
+    AppLink,
+    IconArrowRight,
   },
 
   // data() {
@@ -24,10 +30,37 @@ export default defineComponent({
 </script>
 
 <template>
-  <app-pages />
+  <app-breadcrumbs>
+    <pages-item>
+      <router-link to="/">
+        <app-link>
+          Головна
+        </app-link>
+      </router-link>
+    </pages-item>
+
+    <pages-item>
+      <icon-arrow-right class="icon-arrow" />
+    </pages-item>
+
+    <pages-item>
+      <router-link to="/cart">
+        <app-link class="active-link">
+          Кошик
+        </app-link>
+      </router-link>
+    </pages-item>
+  </app-breadcrumbs>
   <the-checkout />
 </template>
 
 <style scoped>
-
+  .active-link {
+    color: var(--color-grey);
+  }
+  .icon-arrow {
+    width: 6px;
+    height: 6px;
+    fill: var(--color-black);
+  }
 </style>
