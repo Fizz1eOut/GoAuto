71e5bea23628762b98960c58deae654c692ac27e
FizzleOut <fizzleout1337@gmail.com>
2024-05-20 20:20:15 +0200

    work in progress

--- src/components/Content/OurWorks.vue
+++ src/components/Content/OurWorks.vue
@@ -11,19 +11,19 @@ export default defineComponent({
 
   data() {
     return {
-      images: []
+      // images: []
     };
   },
 
-  mounted() {
-    for (let i = 1; i <= 12; i++) {
-      const imageName = `../../assets/images/our-products/${i}.svg`;
-      // console.log(imageName)
-      const imageUrl = new URL(imageName, import.meta.url).href;
-      // console.log(imageUrl)
-      this.images.push({ id: i, src: imageUrl, alt: `Зображення ${i}` });
-    }
-  }
+  // mounted() {
+  //   for (let i = 1; i <= 12; i++) {
+  //     const imageName = `../../assets/images/our-products/${i}.svg`;
+  //     // console.log(imageName)
+  //     const imageUrl = new URL(imageName, import.meta.url).href;
+  //     // console.log(imageUrl)
+  //     this.images.push({ id: i, src: imageUrl, alt: `Зображення ${i}` });
+  //   }
+  // }
 });
 </script>
 
@@ -33,8 +33,57 @@ export default defineComponent({
       Наші роботи
     </AppTitle>
     <div class="our-works__body">
-      <div v-for="image in images" :key="image.id" class="our-works__image">
+      <!-- <div v-for="image in images" :key="image.id" class="our-works__image">
         <img :src="image.src" :alt="image.alt">
+        <img src="" alt="">
+      </div> -->
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/1.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/2.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/3.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/4.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/5.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/6.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/7.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/8.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/9.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/10.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/11.svg" alt="Зображення">
+      </div>
+
+      <div class="our-works__image">
+        <img src="../../assets/images/our-products/12.svg" alt="Зображення">
       </div>
     </div>
   </div>
