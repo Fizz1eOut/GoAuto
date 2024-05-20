6e08991ce7f5bfd98ac6cf854ea6ee3a1559f893
FizzleOut <fizzleout1337@gmail.com>
2024-05-20 19:53:04 +0200

    Added dynamic addition of images to the OurWorks component

--- src/components/Content/OurWorks.vue
+++ src/components/Content/OurWorks.vue
@@ -7,6 +7,22 @@ export default defineComponent({
 
   components: {
     AppTitle
+  },
+
+  data() {
+    return {
+      images: []
+    };
+  },
+
+  mounted() {
+    for (let i = 1; i <= 12; i++) {
+      const imageName = `../../assets/images/our-products/${i}.svg`;
+      // console.log(imageName)
+      const imageUrl = new URL(imageName, import.meta.url).href;
+      // console.log(imageUrl)
+      this.images.push({ id: i, src: imageUrl, alt: `Зображення ${i}` });
+    }
   }
 });
 </script>
@@ -17,52 +33,8 @@ export default defineComponent({
       Наші роботи
     </AppTitle>
     <div class="our-works__body">
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/1.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/2.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/3.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/4.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/5.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/6.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/7.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/8.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/9.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/10.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/11.svg" alt="Зображення">
-      </div>
-
-      <div class="our-works__image">
-        <img src="../../assets/images/our-products/12.svg" alt="Зображення">
+      <div v-for="image in images" :key="image.id" class="our-works__image">
+        <img :src="image.src" :alt="image.alt">
       </div>
     </div>
   </div>
