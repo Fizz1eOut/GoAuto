c8c3a0629a11c115289ac209adf46ea317ce3a2e
FizzleOut <fizzleout1337@gmail.com>
2024-07-02 17:22:28 +0200

    Changes to the “breadcrumbs” component; added dynamic display of transition to a category when on a product page.

--- src/components/Base/AppBreadcrumbs.vue
+++ src/components/Base/AppBreadcrumbs.vue
@@ -3,7 +3,7 @@ import { defineComponent } from 'vue';
 import { useRoute } from 'vue-router';
 import AppLink from '@/components/Base/AppLink.vue';
 import IconArrowRight from '@/components/icons/IconArrowRight.vue';
-import BreadcrumbItem from '@/components/Content/BreadcrumbItem.vue';
+import categoriesData from '@/api/categories.json';
 
 export default defineComponent({
   name: 'AppBreadcrumbs',
@@ -11,7 +11,13 @@ export default defineComponent({
   components: {
     AppLink,
     IconArrowRight,
-    BreadcrumbItem,
+  },
+
+  props: {
+    product: {
+      type: Object,
+      default: null
+    }
   },
 
   data() {
@@ -21,23 +27,34 @@ export default defineComponent({
   },
 
   computed: {
+    categoryTitle() {
+      if (this.product) {
+        return categoriesData.find(category => category.id === this.product.category);
+      }
+      return null;
+    },
+
     breadcrumbs() {
-      const matched = this.route.matched.filter(({ meta }) => !!meta.title);
+      const matched = this.route.matched.filter(({ meta }) => !!meta.title); // Фильтруем маршруты с наличием мета-тега title
       const breadcrumbs = matched.map(routeRecord => ({
-        title: routeRecord.meta.title,
-        path: routeRecord.path,
+        title: routeRecord.meta.title, // Получаем title из мета-тега маршрута
+        path: routeRecord.path, // Получаем путь маршрута
       }));
-      
-      // Добавляем главную страницу в начало массива хлебных крошек
-      breadcrumbs.unshift({ title: 'Головна', path: '/' });
-      
-      // Если текущий маршрут - это страница продукта, добавляем динамическое название
-      if (this.route.name === 'ProductDetailView') {
-        const productAlias = this.route.params.alias;
-        breadcrumbs.push({ title: productAlias, path: this.route.path });
+
+      breadcrumbs.unshift({ title: 'Головна', path: '/' }); // Добавляем элемент для главной страницы в начало массива
+      if (this.route.name === 'ProductDetailView' && this.product) { // Если текущий маршрут - это страница продукта и продукт передан
+        const category = this.categoryTitle; // Получаем категорию продукта
+        if (category) { // Если категория найдена
+          if (category.title === 'Шини') { // Если категория - "Шини"
+            breadcrumbs.push({ title: 'Шини', path: '/tires' }); // Добавляем элемент "Шини" в хлебные крошки
+          } else if (category.title === 'Диски') { // Если категория - "Диски"
+            breadcrumbs.push({ title: 'Диски', path: '/rims' }); // Добавляем элемент "Диски" в хлебные крошки
+          }
+          breadcrumbs.push({ title: this.product.title, path: this.route.path }); // Добавляем элемент с названием продукта в хлебные крошки
+        }
       }
 
-      return breadcrumbs;
+      return breadcrumbs; // Возвращаем массив хлебных крошек
     }
   }
 });
@@ -46,14 +63,18 @@ export default defineComponent({
 <template>
   <div class="breadcrumbs">
     <div class="breadcrumbs__items">
-      <breadcrumb-item v-for="(breadcrumb, index) in breadcrumbs" :key="index">
+      <div 
+        v-for="(breadcrumb, index) in breadcrumbs" 
+        :key="index" 
+        class="breadcrumb-item"
+      >
         <router-link :to="breadcrumb.path">
-          <app-link :class="{ 'active-link': index === breadcrumbs.length - 1 }">
+          <app-link :class="{ 'active-link': index === breadcrumbs.length - 1 }" class="breadcrumbs__link">
             {{ breadcrumb.title }}
           </app-link>
         </router-link>
         <icon-arrow-right v-if="index < breadcrumbs.length - 1" class="icon-arrow" />
-      </breadcrumb-item>
+      </div>
     </div>
   </div>
 </template>
@@ -63,5 +84,16 @@ export default defineComponent({
     display: flex;
     align-items: center;
     gap: 5px;
+  }
+  .breadcrumbs__link {
+    margin-right: 5px;
+  }
+  .active-link {
+    color: var(--color-grey);
+  }
+  .icon-arrow {
+    width: 8px;
+    height: 8px;
+    fill: var(--color-black);
   }
 </style>
--- src/pages/ProductDetailView.vue
+++ src/pages/ProductDetailView.vue
@@ -1,11 +1,7 @@
 <script>
 import AppBreadcrumbs from '@/components/Base/AppBreadcrumbs.vue';
-import BreadcrumbItem from '@/components/Content/BreadcrumbItem.vue';
-import AppLink from '@/components/Base/AppLink.vue';
-import IconArrowRight from '@/components/icons/IconArrowRight.vue';
 import CardProduct from '@/components/Base/AppCardProduct.vue';
 import productsData from '@/api/products.json';
-import categoriesData from '@/api/categories.json';
 import AppFeaturedProduct from '@/components/Base/AppFeaturedProduct.vue';
 import TheConsultation from '@/components/Content/TheConsultation.vue';
 import { defineComponent } from 'vue';
@@ -15,13 +11,11 @@ export default defineComponent({
 
   components: {
     AppBreadcrumbs,
-    BreadcrumbItem,
-    AppLink,
-    IconArrowRight,
     CardProduct,
     AppFeaturedProduct,
     TheConsultation
   },
+  
 
   computed: {
     findProduct() {
@@ -30,74 +24,17 @@ export default defineComponent({
     // Находим данные о товаре по названию
     return productsData.find(product => product.alias === productName);
   },
-    categoryTitle() {
-      // console.log(this.findProduct);
-      return categoriesData.find(category => category.id === this.findProduct.category)
-    }
   }
 });
 </script>
 
 <template>
-  <app-breadcrumbs class="breadcrumbs">
-    <router-link to="/">
-      <breadcrumb-item>
-        <app-link>
-          Головна
-        </app-link>
-      </breadcrumb-item>
-    </router-link>
-
-    <breadcrumb-item>
-      <icon-arrow-right class="icon-arrow" />
-    </breadcrumb-item>
-
-    <template v-if="categoryTitle.title === 'Шини'">
-      <router-link to="/tires">
-        <breadcrumb-item>
-          <app-link>
-            {{ categoryTitle.title }}
-          </app-link>
-        </breadcrumb-item>
-      </router-link>
-    </template>
-
-    <template v-if="categoryTitle.title === 'Диски'">
-      <router-link to="/rims">
-        <breadcrumb-item>
-          <app-link>
-            {{ categoryTitle.title }}
-          </app-link>
-        </breadcrumb-item>
-      </router-link>
-    </template>
-
-    <breadcrumb-item>
-      <icon-arrow-right class="icon-arrow" />
-    </breadcrumb-item>
-
-    <breadcrumb-item>
-      <app-link class="active-link">
-        {{ findProduct.title }}
-      </app-link>
-    </breadcrumb-item>
-  </app-breadcrumbs>
-
+  <app-breadcrumbs :product="findProduct" />
   <card-product :product="findProduct" />
   <app-featured-product />
   <the-consultation />
 </template>
 
 <style scoped>
-  .breadcrumbs {
-    margin-bottom: 30px;
-  }
-  .active-link {
-    color: var(--color-grey);
-  }
-  .icon-arrow {
-    width: 6px;
-    height: 6px;
-    fill: var(--color-black);
-  }
+
 </style>
